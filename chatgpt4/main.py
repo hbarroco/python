b65063140ad4888d7e8da701e97b9247e08aebc8
Helio Barroco <heliobarroco@gmail.com>
2023-05-03 23:31:09 -0300

    update main.py

--- chatgpt4/main.py
+++ chatgpt4/main.py
@@ -1,1 +1,11 @@
-print('Hello only test')
+import openai
+openai.api_key = "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"
+
+resultado = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+    messages = [{"role": "user", "content": "What is the between Campinas and São Paulo"}]
+)
+
+print(resultado)
+
+#print('Hello only test')
